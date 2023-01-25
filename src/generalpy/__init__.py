77d5c69de65ee7c8dcc4d5d556aef5fcf5285da1
Hussain <hussainabbas5416@outlook.com>
2023-01-25 10:49:57 +0530

    get_new_path can be used directly via library

--- src/generalpy/__init__.py
+++ src/generalpy/__init__.py
@@ -1,8 +1,11 @@
 # Importing here, So these classes can also be used directly
-from .settings import Settings
 from .custom_logging import LevelFormatter, CustomLogging
 from .decorator import (
     run_threaded,
     combine_single_items,
     conditional
 )
+from .files import (
+    get_new_path
+)
+from .settings import Settings
