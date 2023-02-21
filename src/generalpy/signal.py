6b2268b92968023701a33285719cdef839079aa2
Hussain <hussainabbas5416@outlook.com>
2023-02-21 13:04:55 +0530

    Added threading, daemon, logger options:         - `threaded`: Emit the signal in a new thread         - `daemon`: If `threaded`, thread should be daemon or not         - `logger`: `Logger` to be used for logging

--- src/generalpy/signal.py
+++ src/generalpy/signal.py
@@ -1,8 +1,16 @@
 """
 Module for signal and slot related functions
 """
+from logging import Logger
+import logging
 from typing import Any, Callable
 
+"""
+Items imported inside functions/classes
+
+- from .decorator import run_threaded
+- from .general import generate_repr_str
+"""
 
 
 
@@ -15,21 +23,39 @@ class Signal:
     
     Args:
         - `name`: Name of the signal
+        - `threaded`: Emit the signal in a new thread
+        - `daemon`: If `threaded`, thread should be daemon or not
+        - `logger`: `Logger` to be used for logging
     """
 
-    def __init__(self, name = 'Signal'):
+    def __init__(
+        self, name = 'Signal',
+        threaded = False,
+        daemon = True,
+        logger: Logger | None = None
+    ):
+        # Mods
+        if logger is None:
+            from ._utils import _get_basic_logger
+            logger = _get_basic_logger()
+
+        # Args
         self._callback: Callable | None = None
         self.name = name
+        self.threaded = threaded
+        self.daemon = daemon
+        self.logger = logger
     
     def __repr__(self) -> str:
         from .general import generate_repr_str
         return generate_repr_str(
-            self, 'name'
+            self, 'name', 'threaded', 'daemon', 'logger'
         )
 
     def connect(self, callback: Callable):
         """ Connect `callback` to signal """
         self._callback = callback
+        self.logger.debug(f'Callback "{callback.__name__}" setted for {self.name} signal')
 
     def disconnect(self, callback: Callable, ignoreError = False):
         """ 
@@ -39,8 +65,10 @@ class Signal:
         """
         if self._callback != callback:
             if ignoreError:
+                self.logger.debug(f'"{callback.__name__}" is not connected to "{self.name}" signal')
                 return
             raise ValueError(f'"{callback.__name__}" is not connected to "{self.name}" signal')
+        self.logger.debug(f'Removed "{callback.__name__}" from {self.name} signal')
         self._callback = None
 
     def emit(self, *args: Any, **kwargs: Any):
@@ -49,5 +77,13 @@ class Signal:
         - Connect callback will be called with the given arguments (if present)
         """
         if self._callback:
-            self._callback(*args, **kwargs)
+            if self.threaded:
+                from .decorator import run_threaded
+                run_threaded(
+                    daemon=self.daemon,
+                    name=f'Signal Thread {self._callback.__name__}',
+                    logger=self.logger,
+                )(self._callback)(*args, **kwargs)
+            else:
+                self._callback(*args, **kwargs)
 
