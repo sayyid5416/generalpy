18cff2136b5ed1f36b187e640cf1911240f5d923
Hussain <hussainabbas5416@outlook.com>
2023-02-21 13:34:41 +0530

    added args and kwargs support during connect method

--- src/generalpy/signal.py
+++ src/generalpy/signal.py
@@ -40,22 +40,35 @@ class Signal:
             logger = _get_basic_logger()
 
         # Args
-        self._callback: Callable | None = None
         self.name = name
         self.threaded = threaded
         self.daemon = daemon
         self.logger = logger
+
+        # Data
+        self._callback: Callable | None = None
+        self.cb_args = tuple()
+        self.cb_kwargs = dict()
+        self._set_default_values()
     
     def __repr__(self) -> str:
         from .general import generate_repr_str
         return generate_repr_str(
             self, 'name', 'threaded', 'daemon', 'logger'
         )
+    
+    def _set_default_values(self):
+        """ Sets the default values for items """
+        self._callback = None
+        self.cb_args = tuple()
+        self.cb_kwargs = dict()
 
-    def connect(self, callback: Callable):
+    def connect(self, callback: Callable, *args, **kwargs):
         """ Connect `callback` to signal """
         self._callback = callback
-        self.logger.debug(f'Callback "{callback.__name__}" setted for "{self.name}" signal')
+        self.cb_args = args
+        self.cb_kwargs = kwargs
+        self.logger.debug(f'Callback "{callback.__name__}({args=}, {kwargs=})" setted for "{self.name}" signal')
 
     def disconnect(self, callback: Callable, ignoreError = False):
         """ 
@@ -69,15 +82,23 @@ class Signal:
                 return
             raise ValueError(f'"{callback.__name__}" is not connected to "{self.name}" signal')
         self.logger.debug(f'Removed "{callback.__name__}" from "{self.name}" signal')
-        self._callback = None
+        self._set_default_values()
 
     def emit(self, threaded: bool | None = None, *args: Any, **kwargs: Any):
         """
         Emit the signal with the given arguments
         - Connected callback will run with `args` and `kwargs` (if present)
+            - These `args` and `kwargs` be passed along with those which were set in `.connect(...)` method
+            - These `kwargs` will take precedence (in case of duplication) over those which were set in `.connect(...)` method
+            - Final Args would be like `(earlier_args, these_args, final_kwargs)`
         - `threaded`: If `True/False`, It overrides the behavior of `threaded` setted during `Signal(threaded=..., ...)`
         """
+        # Modify
         _threaded = self.threaded if threaded is None else threaded
+        _args = self.cb_args + args
+        _kwargs = {**self.cb_kwargs, **kwargs}
+
+        # Run
         if self._callback:
             if _threaded:
                 from .decorator import run_threaded
@@ -85,7 +106,7 @@ class Signal:
                     daemon=self.daemon,
                     name=f'Signal Thread {self._callback.__name__}',
                     logger=self.logger,
-                )(self._callback)(*args, **kwargs)
+                )(self._callback)(*_args, **_kwargs)
             else:
-                self._callback(*args, **kwargs)
+                self._callback(*_args, **_kwargs)
 
